450243b60f1b7945b81db2d538ec7fcc7117dbd1
嘤熊嚎杰 <wanlegame@live.com>
2022-03-07 17:17:03 +0800

    0.1.1

--- source/conf.py
+++ source/conf.py
@@ -31,8 +31,7 @@ release = 'v0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark',
-    'sphinx_markdown_tables'
+    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
